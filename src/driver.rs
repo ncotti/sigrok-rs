2608ed905a39c80490c34475abde00e4b43a7f5c
nicolas.cotti <nicolas.cotti@indiesemi.com>
2026-09-05 15:54:56 -0300

    Free device list scanned by driver

--- src/driver.rs
+++ src/driver.rs
@@ -88,9 +88,9 @@ impl Driver {
 
         let p_devices: Vec<*mut sr_dev_inst> = gslist_to_vec(device_list);
 
-        Ok(p_devices)
+        unsafe { glib::ffi::g_slist_free(device_list.cast()) };
 
-        // unsafe { glib::ffi::g_slist_free(device_list.cast()) };
+        Ok(p_devices)
     }
 
     /// Returns the driver's name.
